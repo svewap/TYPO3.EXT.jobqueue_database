72a809a8f8b361f4cf357931a173402626092ace
Sven Wappler <info@wappler.systems>
2021-11-08 00:09:23 +0100

    convert array to short syntax

--- Configuration/TCA/tx_jobqueuedatabase_domain_model_job.php
+++ Configuration/TCA/tx_jobqueuedatabase_domain_model_job.php
@@ -1,96 +1,96 @@
 <?php
-return array(
-    'ctrl' => array(
+return [
+    'ctrl' => [
         'title' => 'LLL:EXT:jobqueue_database/Resources/Private/Language/locallang_db.xlf:tx_jobqueuedatabase_domain_model_job',
         'label' => 'tstamp',
         'tstamp' => 'tstamp',
         'crdate' => 'crdate',
         'dividers2tabs' => true,
         'hideTable' => true,
-        'enablecolumns' => array(
+        'enablecolumns' => [
 
-        ),
+        ],
         'searchFields' => 'uid,queue_name,state,attemps,starttime,tstamp,',
         'iconfile' => 'EXT:jobqueue_database/Resources/Public/Icons/tx_jobqueuedatabase_domain_model_job.gif',
         'readOnly' => true,
         'rootLevel' => 1,
-    ),
-    'interface' => array(
+    ],
+    'interface' => [
         'showRecordFieldList' => 'queue_name, payload, state, attemps, starttime, tstamp',
-    ),
-    'types' => array(
-        '1' => array('showitem' => 'queue_name, payload, state, attemps, starttime, tstamp, '),
-    ),
-    'palettes' => array(
-        '1' => array('showitem' => ''),
-    ),
-    'columns' => array(
+    ],
+    'types' => [
+        '1' => ['showitem' => 'queue_name, payload, state, attemps, starttime, tstamp, '],
+    ],
+    'palettes' => [
+        '1' => ['showitem' => ''],
+    ],
+    'columns' => [
 
-        'queue_name' => array(
+        'queue_name' => [
             'exclude' => 0,
             'label' => 'LLL:EXT:jobqueue_database/Resources/Private/Language/locallang_db.xlf:tx_jobqueuedatabase_domain_model_job.queue_name',
-            'config' => array(
+            'config' => [
                 'readOnly' => true,
                 'type' => 'input',
                 'size' => 30,
                 'eval' => 'trim',
-            ),
-        ),
-        'payload' => array(
+            ],
+        ],
+        'payload' => [
             'exclude' => 0,
             'label' => 'LLL:EXT:jobqueue_database/Resources/Private/Language/locallang_db.xlf:tx_jobqueuedatabase_domain_model_job.payload',
-            'config' => array(
+            'config' => [
                 'readOnly' => true,
                 'type' => 'text',
                 'cols' => 40,
                 'rows' => 15,
                 'eval' => 'trim',
-            ),
-        ),
-        'state' => array(
+            ],
+        ],
+        'state' => [
             'exclude' => 0,
             'label' => 'LLL:EXT:jobqueue_database/Resources/Private/Language/locallang_db.xlf:tx_jobqueuedatabase_domain_model_job.state',
-            'config' => array(
+            'config' => [
                 'readOnly' => true,
                 'type' => 'input',
                 'size' => 4,
                 'eval' => 'int',
-            ),
-        ),
-        'attemps' => array(
+            ],
+        ],
+        'attemps' => [
             'exclude' => 0,
             'label' => 'LLL:EXT:jobqueue_database/Resources/Private/Language/locallang_db.xlf:tx_jobqueuedatabase_domain_model_job.attemps',
-            'config' => array(
+            'config' => [
                 'readOnly' => true,
                 'type' => 'input',
                 'size' => 4,
                 'eval' => 'int',
-            ),
-        ),
-        'starttime' => array(
+            ],
+        ],
+        'starttime' => [
             'exclude' => 0,
             'label' => 'LLL:EXT:jobqueue_database/Resources/Private/Language/locallang_db.xlf:tx_jobqueuedatabase_domain_model_job.starttime',
-            'config' => array(
+            'config' => [
                 'readOnly' => true,
                 'type' => 'input',
                 'size' => 10,
                 'eval' => 'datetime',
                 'checkbox' => 1,
                 'default' => time(),
-            ),
-        ),
-        'tstamp' => array(
+            ],
+        ],
+        'tstamp' => [
             'exclude' => 0,
             'label' => 'LLL:EXT:jobqueue_database/Resources/Private/Language/locallang_db.xlf:tx_jobqueuedatabase_domain_model_job.tstamp',
-            'config' => array(
+            'config' => [
                 'readOnly' => true,
                 'type' => 'input',
                 'size' => 10,
                 'eval' => 'datetime',
                 'checkbox' => 1,
                 'default' => time(),
-            ),
-        ),
+            ],
+        ],
 
-    ),
-);
+    ],
+];
